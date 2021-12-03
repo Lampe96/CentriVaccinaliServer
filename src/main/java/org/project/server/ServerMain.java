75a4a9d64ec62758b42c69fb0932f554dcd00731
Fede22dev <79111932+Fede22dev@users.noreply.github.com>
2021-12-03 18:47:18 +0100

    Sistemata rmi per funzionare su più pc

--- src/main/java/org/project/server/ServerMain.java
+++ src/main/java/org/project/server/ServerMain.java
@@ -3,9 +3,10 @@ package org.project.server;
 import org.project.database.DbHelper;
 import org.project.database.Statements;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
 import java.sql.SQLException;
 
 /**
@@ -30,27 +31,32 @@ public class ServerMain {
     public static void main(String[] args) {
         try {
             ServerImpl server = new ServerImpl();
-            Registry reg = LocateRegistry.createRegistry(Server.PORT);
-            reg.rebind(Server.NAME, server);
-            System.out.println("SERVER PRONTO");
 
             try {
-                Statements.initializeDb();
+                LocateRegistry.createRegistry(Server.PORT);
+                Naming.rebind("rmi://localhost:" + Server.PORT + "/" + Server.NAME, server);
+                System.out.println("SERVER PRONTO");
 
-                Runtime.getRuntime().addShutdownHook(
-                        new Thread(() -> {
-                            try {
-                                DbHelper.closeStatement();
-                                DbHelper.closeConnection();
-                            } catch (SQLException e) {
-                                e.printStackTrace();
-                                System.exit(-1);
-                            }
-                        })
-                );
-            } catch (SQLException e) {
+                try {
+                    Statements.initializeDb();
+
+                    Runtime.getRuntime().addShutdownHook(
+                            new Thread(() -> {
+                                try {
+                                    DbHelper.closeStatement();
+                                    DbHelper.closeConnection();
+                                } catch (SQLException e) {
+                                    e.printStackTrace();
+                                    System.exit(-1);
+                                }
+                            })
+                    );
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    System.exit(-1);
+                }
+            } catch (MalformedURLException e) {
                 e.printStackTrace();
-                System.exit(-1);
             }
         } catch (RemoteException e) {
             e.printStackTrace();
